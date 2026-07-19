d9675382e2d526c1ccf14c67261891de356cb10a
computerisargaran-svg <computerisargaran@gmail.com>
2026-07-19 01:14:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+function openModal(id){
+    document.getElementById(id).style.display="block";
+}
+
+function closeModal(id){
+    document.getElementById(id).style.display="none";
+}
+
+function closeOutside(event){
+    if(event.target.classList.contains("modal")){
+        event.target.style.display="none";
+    }
+}
+
+document.addEventListener("keydown",function(event){
+
+    if(event.key==="Escape"){
+
+        let modals=document.querySelectorAll(".modal");
+
+        modals.forEach(function(modal){
+            modal.style.display="none";
+        });
+
+    }
+
+});
